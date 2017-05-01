1516ae3cf5578c80e85e38e1e02150a579598ec0
rmad17 <souravbasu17@gmail.com>
2017-05-02 00:26:59 +0530

    Updated setup

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@
 from setuptools import setup
 
 setup(
-    name='pipinstall',
-    version='0.0.2',
+    name='pyreqs',
+    version='0.0.1',
     description='Better manage python requirements file',
     author='Sourav Basu',
     author_email='souravbasu17@gmail.com',
-    url='https://github.com/rmad17/pipinstall',
-    py_modules=['pipinstall'],
+    url='https://github.com/rmad17/pyreqs',
+    py_modules=['pyreqs'],
     install_requires=[
         'click', 'sh'
     ],
     entry_points='''
         [console_scripts]
-        pipinstall=pipinstall:pipinstall
+        pyreqs=pyreqs:pyreqs
     ''',
 )
